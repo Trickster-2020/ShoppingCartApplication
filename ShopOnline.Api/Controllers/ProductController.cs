59f81eaa7e6cee0fec57a85db1c70986483c93e2
Sujan Karna <karna.sujan52@gmail.com>
2023-10-09 21:42:32 +0545

    Product Details configured

--- ShopOnline.Api/Controllers/ProductController.cs
+++ ShopOnline.Api/Controllers/ProductController.cs
@@ -17,15 +17,38 @@ namespace ShopOnline.Api.Controllers
         {
             this.productRepository = productRepository;
         }
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<ProductDto>> GetItem(int id)
+        {
+            try
+            {
+                var product = await this.productRepository.GetItem(id);
+                
+
+                if(product == null ) return BadRequest();
+                else
+                {
+                    var productCategory = await this.productRepository.GetCategory(product.CategoryId);
+                    var productDto = product.ConvertToDto(productCategory);
+                    return Ok(productDto);
+                }
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Error retrieving data from the database");
+            }
+        }
         [HttpGet]
         public async Task<ActionResult<IEnumerable<ProductDto>>> GetItems()
         {
             try
             {
                 var products = await this.productRepository.GetItems();
-                var productCategories  = await this.productRepository.GetCategories();
+                var productCategories = await this.productRepository.GetCategories();
 
-                if(products == null || productCategories == null) return NotFound();
+                if (products == null || productCategories == null) return NotFound();
                 else
                 {
                     var productDtos = products.ConvertToDto(productCategories);
